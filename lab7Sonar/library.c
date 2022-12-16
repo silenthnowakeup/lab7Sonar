3237709678b392ffcf46e3a3a62deea9956c3e94
Stas <112703038+silenthnowakeup@users.noreply.github.com>
2022-12-17 01:04:58 +0300

    Update library.c

--- lab7Sonar/library.c
+++ lab7Sonar/library.c
@@ -139,7 +139,6 @@ int contCheck() {
 void Task1()
 {
     char *str;
-    str = NULL;
     printf("Input your string::");
     str = getStr(str);
     printf("\nsumma = %d \n", myf(str));
@@ -148,9 +147,7 @@ void Task1()
 void Task2()
 {
     char* s1;
-    s1 = NULL;
     char* s2;
-    s2 = NULL;
     int k;
     int m;
     printf("Input first string::");
